9cbda2621e8602a92d7f566603fb0dd86ca69c8b
Derrick Abuga <derrickabuga06@gmail.com>
2025-06-11 07:24:11 +0300

    First Commit

---
+++ flappyball.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+
+#INITIALIZE PYGAME
+pygame.init()
+
+#SET SCREEN DIMENSIONS
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 600
+
+pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))      #CREATE GAME WINDOW
+pygame.display.set_caption("FLAPPY BALL")       #WINDOW TITLE
+
+#Initialize pygame
+#Define the game window
+#add a background
+#set the frame rate
